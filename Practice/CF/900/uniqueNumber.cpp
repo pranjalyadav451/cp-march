8b09cecdfce6a2dfac65461f59e1a51f9610a5b2
pranjalyadav451 <2019uec1008@mnit.ac.in>
2021-03-19 06:44:19 +0530

    unique Number

---
+++ Practice/CF/900/uniqueNumber.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vec vector
+#define MOD 1000000007
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define f first
+#define s second
+
+#define sz(x) int((x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define trav(a, x) for (auto &a : x)
+#define fo(i, n) for (i = 0; i < n; i++)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+
+using namespace std;
+
+string solve(ll x)
+{
+    string ans = "";
+
+    if (x > 45)
+        return "-1";
+    else
+    {
+        int i = 9;
+
+        while (x > 0)
+        {
+            if (i < x)
+                ans.push_back(i + '0');
+            else
+            {
+                ans.push_back(x + '0');
+                i = x;
+            }
+            x -= i;
+            i--;
+        }
+    }
+
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        ll x;
+        cin >> x;
+        cout << solve(x) << endl;
+    }
+}
